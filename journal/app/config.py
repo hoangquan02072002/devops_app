2e67da30689b0257c8d7a4c5331d2732c7d72446
MelnikovDMA <starwarriordm@gmail.com>
2023-11-15 22:48:46 +0300

    aaa

---
+++ journal/app/config.py
@@ -0,0 +1,8 @@
+SECRET_KEY = 'e341e6698cb20dd889d040a9be7d5fc129cb06255f349bd6ea3f901afe8d61b4'
+
+ip="localhost"
+PGUSER="postgres"
+PGPASSWORD="linky1337"
+DATABASE="DevOps"
+
+POSTGRES_URI = f'postgresql://{PGUSER}:{PGPASSWORD}@{ip}/{DATABASE}'
