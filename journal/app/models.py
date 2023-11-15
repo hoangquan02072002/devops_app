c1825a2707cc8518a6287736cc453fd66c521a4a
MelnikovDMA <starwarriordm@gmail.com>
2023-11-15 22:38:51 +0300

    adding code

--- journal/app/models.py
+++ journal/app/models.py
@@ -16,6 +16,8 @@ class Student(db.Model):
     def __repr__(self):
         return '<Student %r>' % self.lastname
     
+    # __table_args__ = {'extend_existing': True}
+    
 class PlanOfStudy(db.Model):
     __tablename__ = 'planofstudy'
 
@@ -25,6 +27,9 @@ class PlanOfStudy(db.Model):
     semester = db.Column(db.Integer, nullable=False)
     hours = db.Column(db.Integer, nullable=False)
     exam_or_test = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return self.discipline
 
 class Gradebook(db.Model):
     __tablename__ = 'gradebook'
@@ -37,3 +42,55 @@ class Gradebook(db.Model):
 
     student = db.relationship('Student')
     planofstudy = db.relationship('PlanOfStudy')
+
+# def fill_db():
+#     users = [
+#         Student(lastname='Иванов', name='Иван', surname='Иванович', admission_year=2018, group=101, education_form='дневная'),
+#         Student(lastname='Петрова', name='Анна', surname='Сергеевна', admission_year=2020, group=203, education_form='вечерняя'),
+#         Student(lastname='Смирнов', name='Дмитрий', surname='Александрович', admission_year=2019, group=305, education_form='заочная'),
+#         Student(lastname='Козлова', name='Екатерина', surname='Владимировна', admission_year=2017, group=102, education_form='дневная'),
+#         Student(lastname='Морозов', name='Павел', surname='Андреевич', admission_year=2021, group=204, education_form='вечерняя'),
+#         Student(lastname='Иванова', name='Ивана', surname='Ивановича', admission_year=2018, group=102, education_form='дневная'),
+#         Student(lastname='Петрова', name='Анна', surname='Сергеевна', admission_year=2020, group=203, education_form='вечерняя'),
+#         Student(lastname='Смирнов', name='Дмитрий', surname='Александрович', admission_year=2019, group=305, education_form='заочная'),
+#         Student(lastname='Козлова', name='Екатерина', surname='Владимировна', admission_year=2017, group=102, education_form='дневная'),
+#         Student(lastname='Морозов', name='Павел', surname='Андреевич', admission_year=2021, group=204, education_form='вечерняя'),
+#         Student(lastname='Иванов', name='Иван', surname='Иванович', admission_year=2018, group=101, education_form='дневная'),
+#         Student(lastname='Петрова', name='Анна', surname='Сергеевна', admission_year=2020, group=203, education_form='вечерняя'),
+#         Student(lastname='Смирнов', name='Дмитрий', surname='Александрович', admission_year=2019, group=305, education_form='заочная'),
+#         Student(lastname='Козлова', name='Екатерина', surname='Владимировна', admission_year=2017, group=102, education_form='дневная'),
+#         Student(lastname='Морозов', name='Павел', surname='Андреевич', admission_year=2021, group=204, education_form='вечерняя'),
+#         Student(lastname='Иванов', name='Иван', surname='Иванович', admission_year=2018, group=101, education_form='дневная'),
+#         Student(lastname='Петрова', name='Анна', surname='Сергеевна', admission_year=2020, group=203, education_form='вечерняя'),
+#         Student(lastname='Смирнов', name='Дмитрий', surname='Александрович', admission_year=2019, group=305, education_form='заочная'),
+#         Student(lastname='Козлова', name='Екатерина', surname='Владимировна', admission_year=2017, group=102, education_form='дневная'),
+#         Student(lastname='Морозов', name='Павел', surname='Андреевич', admission_year=2021, group=204, education_form='вечерняя'),
+#     ]
+
+#     # Добавьте объекты в сессию и сохраните их в базе данных
+#     for user in users:
+#         db.session.add(user)
+#     db.session.commit()
+
+# def fill_db2():
+#     data = [
+#         PlanOfStudy(speciality="Информатика", discipline="Программирование", semester=3, hours=60, exam_or_test="Экзамен"),
+#         PlanOfStudy(speciality="Математика", discipline="Математический анализ", semester=2, hours=45, exam_or_test="Зачет"),
+#         PlanOfStudy(speciality="Физика", discipline="Общая физика", semester=1, hours=75, exam_or_test="Экзамен"),
+#         PlanOfStudy(speciality="Искусствоведение", discipline="История искусства", semester=4, hours=90, exam_or_test="Экзамен"),
+#         PlanOfStudy(speciality="Экономика", discipline="Экономическая теория", semester=5, hours=120, exam_or_test="Экзамен"),
+#         PlanOfStudy(speciality="Лингвистика", discipline="Английский язык", semester=1,hours= 60, exam_or_test="Зачет"),
+#         PlanOfStudy(speciality="Прикладная математика", discipline="Теория вероятностей и математическая статистика", semester=3, hours=75, exam_or_test="Экзамен"),
+#         PlanOfStudy(speciality="Гуманитарные науки", discipline="Философия", semester=2, hours=45, exam_or_test="Зачет"),
+#         PlanOfStudy(speciality="Менеджмент", discipline="Маркетинг", semester=4, hours=90, exam_or_test="Экзамен"),
+#         PlanOfStudy(speciality="Юриспруденция", discipline="Основы права", semester=1, hours=60, exam_or_test="Зачет"),
+#         PlanOfStudy(speciality="История", discipline="История России", semester=3, hours=70, exam_or_test="Экзамен"),
+#         PlanOfStudy(speciality="Биология", discipline="Генетика", semester=4, hours=80, exam_or_test="Экзамен"),
+#         PlanOfStudy(speciality="Психология", discipline="Общая психология", semester=2, hours=50, exam_or_test="Зачет"),
+#         PlanOfStudy(speciality="Химия", discipline="Неорганическая химия", semester=5, hours=100, exam_or_test="Экзамен"),
+#         PlanOfStudy(speciality="Социология", discipline="Социологические исследования", semester=4, hours=90, exam_or_test="Экзамен"),
+#     ]
+    
+#     for i in data:
+#         db.session.add(i)
+#     db.session.commit()
